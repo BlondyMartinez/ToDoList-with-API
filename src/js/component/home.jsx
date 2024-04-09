990e25c60446abe8b3e9c14fdc325307c3af8fcf
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-09 06:31:08 +0000

    separated part of it into the header component

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,6 @@
 import React, { useState }  from "react";
 import Note from "./note"
-import UserSelect from "./user_list";
+import Header from "./header";
 
 const Home = () => {
     const GUEST = {
@@ -11,18 +11,9 @@ const Home = () => {
 	const [theme, setTheme] = useState('light-theme');
     const [user, setUser] = useState(GUEST);
 
-	const handleClick = () => {
-		setTheme(theme === 'light-theme' ? 'dark-theme' : 'light-theme');
-    };
-
 	return (
 		<div className={`d-flex flex-column align-items-center bg ${theme}`}>
-			<div className="d-flex justify-content-between w-100 p-2">
-				<UserSelect setUser={setUser} GUEST={GUEST} ></UserSelect>
-				<div onClick={handleClick} className="fs-4" role="button"> 
-					{theme == 'light-theme' ? <i className="fa-solid fa-moon"></i> : <i className="fa-solid fa-sun"></i>} 
-				</div>
-			</div>
+			<Header user={user} setUser={setUser} theme={theme} setTheme={setTheme} GUEST={GUEST} />
 			<h1 className="text-center mt-5">todos</h1>
 			<Note user={user} theme={theme} />
 		</div>
